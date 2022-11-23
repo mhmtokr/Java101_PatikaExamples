fa52e18e0fac3efd1abd9974c4bfa5f3f05f36da
mhmtokr <mehmetokur94@hotmail.com>
2022-11-23 20:17:45 +0300

    pratik5

---
+++ src/Examples6.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class Examples6 {
+    public static void main(String[] args) {
+
+        double armut = 2.14, elma = 3.67, domates = 1.11 , muz = 0.95, patlican = 5.00;
+        double armutkg, elmakg, domateskg, muzkg, patlicankg;
+
+        System.out.print("Armut Kaç Kg? ");
+        Scanner input = new Scanner(System.in);
+        armutkg = input.nextDouble();
+
+        System.out.print("Elma Kaç Kg? ");
+        Scanner input1 = new Scanner(System.in);
+        elmakg = input1.nextDouble();
+
+        System.out.print("Domates Kaç Kg? ");
+        Scanner input2 = new Scanner(System.in);
+        domateskg = input2.nextDouble();
+
+        System.out.print("Muz Kaç Kg? ");
+        Scanner input3 = new Scanner(System.in);
+        muzkg = input3.nextDouble();
+
+        System.out.print("Patlıcan Kaç Kg? ");
+        Scanner input4 = new Scanner(System.in);
+        patlicankg = input4.nextDouble();
+
+        double toplam = (armut*armutkg) + (elma*elmakg) + (domates*domateskg) + (muz*muzkg) + (patlican*patlicankg);
+
+        System.out.println("Toplam Tutar: " + toplam);
+
+
+    }
+}
