dcaae6db17a630053e5c4beccbc06cc67cf86298
mhmtokr <mehmetokur94@hotmail.com>
2022-11-25 18:04:01 +0300

    EtkinlikOnerme eklendi

---
+++ src/patikaDev/KosulluIfadelerVeKodBloklari/EtkinlikOnerme.java
@@ -0,0 +1,29 @@
+package patikaDev.KosulluIfadelerVeKodBloklari;
+
+import java.util.Scanner;
+
+public class EtkinlikOnerme {
+    public static void main(String[] args) {
+
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sıcaklık Giriniz: ");
+        heat = input.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Kayak yapabilirsin.");
+        } else if (heat <= 25) {
+            if (heat <= 15) {
+                System.out.println("Sinemaya gidebilirsin.");
+            }
+            if (heat >= 10) {
+                System.out.println("Pikniğe gidebilirsin.");
+            }
+        } else {
+            System.out.println("Yüzmeye gidebilirsin.");
+        }
+
+
+    }
+}
