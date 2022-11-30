ef6b5c959a88b6fcac39d0fcc41173650c522ead
mhmtokr <mehmetokur94@hotmail.com>
2022-11-30 15:19:51 +0300

    ArtikYil eklendi.

---
+++ src/patikaDev/KosulluIfadelerVeKodBloklari/ArtikYil.java
@@ -0,0 +1,29 @@
+package patikaDev.KosulluIfadelerVeKodBloklari;
+
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+
+        int yil;
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Yil Giriniz: ");
+        yil = input.nextInt();
+
+        if (yil % 100 == 0) {
+            if (yil % 400 == 0) {
+                System.out.println(yil + " artik bir yildir.");
+            } else {
+                System.out.println(yil + " artik bir yil degildir.");
+            }
+        } else if (yil % 4 == 0) {
+            System.out.println(yil + " artik bir yildir.");
+        } else {
+            System.out.println(yil + " artik bir yil degildir.");
+        }
+
+
+    }
+}
