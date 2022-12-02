cc97b2b35b2c2e496d3f8821c0ed68460e687ca8
mhmtokr <mehmetokur94@hotmail.com>
2022-12-02 12:15:31 +0300

    Pratik3

---
+++ src/patikaDev/Donguler/Pratik3.java
@@ -0,0 +1,21 @@
+package patikaDev.Donguler;
+
+import java.util.Scanner;
+
+public class Pratik3 {
+    public static void main(String[] args) {
+        int n,f4,f5;
+        Scanner inp = new Scanner(System.in);
+
+        do {
+            System.out.print("Lütfen Bir tamsayı giriniz: ");
+            n = inp.nextInt();
+            for(int i=1,k=1;i<n || k<n; i*=4,k*=5){
+                System.out.println(i + " " +k);
+            }
+        }
+        while(n<0);
+
+
+    }
+}
