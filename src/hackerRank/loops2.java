6e344322aaf074033a1a9c962c3a46b45526d772
mhmtokr <mehmetokur94@hotmail.com>
2022-12-05 19:00:21 +0300

    loops2 eklendi.

---
+++ src/hackerRank/loops2.java
@@ -0,0 +1,32 @@
+package hackerRank;
+
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+class loops2{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        int i,j;
+        for(i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int p=1;
+            int u=p*b;
+            for(j=0;j<n;j++) {
+
+                int temp=a+u;
+                System.out.printf("%d ",temp);
+                p=p*2;
+                u=u+p*b;
+            }
+            System.out.printf("\n");
+        }
+        in.close();
+
+
+    }
+}
