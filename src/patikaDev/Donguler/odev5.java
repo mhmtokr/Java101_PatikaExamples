5226c885f85a1c1f6de6e7ce4e50116fe94e8647
mhmtokr <mehmetokur94@hotmail.com>
2022-12-05 18:56:57 +0300

    Fibonacci eklendi.

---
+++ src/patikaDev/Donguler/odev5.java
@@ -0,0 +1,21 @@
+package patikaDev.Donguler;
+import java.util.Scanner;
+public class odev5 {
+    public static void main(String[] args) {
+
+        int num;
+        int b=0;
+        int result=1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Eleman sayisi giriniz: ");
+        num = input.nextInt();
+        System.out.println(num + "Elemanli Fibonacci serisi: ");
+
+        for(int i=0; i<=(num-1); i++){
+            System.out.println(b + " ");
+            result +=b;
+            b=result-b;
+        }
+
+    }
+}
