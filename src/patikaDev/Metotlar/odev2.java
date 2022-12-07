bbcb46a11cd286f967c45c52d53030bb12698a48
mhmtokr <mehmetokur94@hotmail.com>
2022-12-07 14:54:29 +0300

    PrimeNumber(Recursive) eklendi.

---
+++ src/patikaDev/Metotlar/odev2.java
@@ -0,0 +1,27 @@
+package patikaDev.Metotlar;
+
+import java.util.Scanner;
+
+public class odev2 {
+    static void prime(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number which you wanna see if it's prime or not:");
+        int a = scan.nextInt();
+        int i=2;
+        int sum=0;
+        while(i<a){
+            if(a%i==0){
+                sum+=i;
+            }
+            i++;
+        }
+        if(sum==0){
+            System.out.println(a+" "+"is a prime number.");
+        }else{
+            System.out.println(a+ "  " +"is not a prime number.");
+        }
+    }
+    public static void main(String[] args) {
+        prime();
+    }
+}
