8ecd961ddc8152d971a871dda7f29fa2f52b994b
mhmtokr <mehmetokur94@hotmail.com>
2022-12-13 17:57:43 +0300

    Employee eklendi.

---
+++ src/patikaDev/OOP/Siniflar/SalaryCalc/Main.java
@@ -0,0 +1,8 @@
+package patikaDev.OOP.Siniflar.SalaryCalc;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee kemal = new Employee ("Kemal", 2000.0, 45, 1985);
+        kemal.tostring();
+    }
+}
