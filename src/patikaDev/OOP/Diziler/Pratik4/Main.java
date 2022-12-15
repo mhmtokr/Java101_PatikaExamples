b53f54c5ce81e468f31b13842da44cc768157f94
mhmtokr <mehmetokur94@hotmail.com>
2022-12-15 12:40:03 +0300

    Pratik4 eklendi.

---
+++ src/patikaDev/OOP/Diziler/Pratik4/Main.java
@@ -0,0 +1,26 @@
+package patikaDev.OOP.Diziler.Pratik4;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int [] arr ={10,7,8,8,2,9,8,10,8,1,33,9,1,2,4,4};
+        int []  storage  = new int[arr.length];
+        int startIndex = 0;
+        Arrays.sort(arr);
+
+        for (int i =0; i<arr.length-1; i++) {
+            if (arr[i]==arr[i+1]){
+                storage[startIndex++] = arr[i];
+            }
+        }
+        System.out.println("Tekrar eden çift Sayılar :");
+        for (int i=0;i<storage.length-1;i++){
+            int value = storage[i];
+            if (value != storage[i+1] && value !=0 && value%2==0 ){
+                System.out.print(value + ", ");
+            }
+        }
+    }
+}
